92b751068b61967585b6f0d2d3cc3561c77741c4
You Vanneth <youvannethzzz@gmail.com>
2023-03-24 13:25:17 +0700

    front

--- index.php
+++ index.php
@@ -10,13 +10,33 @@
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta name="description" content="Male_Fashion Template">
+    <meta name="keywords" content="Male_Fashion, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project 3: PHP Project</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Male-Fashion | Template</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap"
+    rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="public/assets/client/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="public/assets/client/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="public/assets/client/css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="public/assets/client/css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="public/assets/client/css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="public/assets/client/css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="public/assets/client/css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="public/assets/client/css/style.css" type="text/css">
 </head>
 <body>
     <?php
         require_once("./views/" . $end_point . "/component/header.php"); // end_point = "front" or "admin"
+        
         require_once($file_path);
+        
+        require_once("./views/" . $end_point . "/component/footer.php");
     ?>
 </body>
 </html>
--- routes.php
+++ routes.php
@@ -2,7 +2,7 @@
 
     class Route{
 
-        static function getRouteFile($relative_path){
+        static function getRouteFile($relative_path = "/"){
             
             $file_path = "./views/front/layout/home.php";
             switch($relative_path){
@@ -11,6 +11,37 @@
                     break;
                 case "/about":
                     $file_path = "./views/front/layout/about.php";
+                    break;
+                case "/blog-details":
+                    $file_path = "./views/front/layout/blog-details.php";
+                    break;
+                case "/blog":
+                    $file_path = "./views/front/layout/blog.php";
+                    break;
+                case "/checkout":
+                    $file_path = "./views/front/layout/checkout.php";
+                    break;
+                case "/contact":
+                    $file_path = "./views/front/layout/contact.php";
+                    break;
+                case "/main":
+                    $file_path = "./views/front/layout/main.php";
+                    break;
+                case "/shop-details":
+                    $file_path = "./views/front/layout/shop-details.php";
+                    break;
+                case "/shop":
+                    $file_path = "./views/front/layout/shop.php";
+                    break;
+                case "/shopping-cart":
+                    $file_path = "./views/front/layout/about.php";
+                    break;
+                case "/admin/login":
+                    $file_path = "./views/front/layout/shop.php";
+                    break;
+                case "/admin/dashboard":
+                    $file_path = "./views/front/layout/about.php";
+                    break;
             }
 
             return $file_path;
